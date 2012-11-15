4f6a9e472dc080dedac900cd56f6a1cfd278e6d8
Jesse Rosalia <jesse.rosalia@gatech.edu>
2012-11-15 00:49:31 -0500

    Implemented test software...add 2 numbers, with command line parameters to specify interface (old or new) and to specify a number representing the number of seconds to delay.

--- src/LookAndFeelDemo.java
+++ src/LookAndFeelDemo.java
@@ -1,44 +1,69 @@
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.metal.DefaultMetalTheme;
 import javax.swing.plaf.metal.MetalLookAndFeel;
-import javax.swing.plaf.metal.OceanTheme;
 
 public class LookAndFeelDemo implements ActionListener {
     private static String labelPrefix = "Number of button clicks: ";
     private int numClicks = 0;
     final JLabel label = new JLabel(labelPrefix + "0    ");
+    private JTextField input1;
+    private JTextField input2;
+    private JTextField ans;
 
     // Specify the look and feel to use by defining the LOOKANDFEEL constant
     // Valid values are: null (use the default), "Metal", "System", "Motif",
     // and "GTK"
-    final static String LOOKANDFEEL = "System";
+    static String LOOKANDFEEL = "System";
     
     // If you choose the Metal L&F, you can also choose a theme.
     // Specify the theme to use by defining the THEME constant
     // Valid values are: "DefaultMetal", "Ocean",  and "Test"
     final static String THEME = "Test";
     
+    static Integer SECDELAY = 5;
+    private static JFrame theFrame;
+    
 
     public Component createComponents() {
-        JButton button = new JButton("I'm a Swing button!");
+        JButton button = new JButton("Run!");
         button.setMnemonic(KeyEvent.VK_I);
         button.addActionListener(this);
         label.setLabelFor(button);
 
-        JPanel pane = new JPanel(new GridLayout(0, 1));
+        JPanel pane   = new JPanel(new GridLayout(0, 1));
+        JPanel eqPane = new JPanel();
+        eqPane.setLayout(new BoxLayout(eqPane, BoxLayout.X_AXIS));
+        this.input1 = new JTextField();
+        eqPane.add(this.input1);
+        eqPane.add(new JLabel(" x "));
+        this.input2 = new JTextField();
+        eqPane.add(this.input2);
+        eqPane.add(new JLabel(" = "));
+        this.ans = new JTextField();
+        this.ans.setEditable(false);
+        eqPane.add(this.ans);
+        
+        pane.add(eqPane);
         pane.add(button);
         pane.add(label);
         pane.setBorder(BorderFactory.createEmptyBorder(
@@ -54,6 +79,57 @@ public class LookAndFeelDemo implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         numClicks++;
         label.setText(labelPrefix + numClicks);
+        try {
+            final int milliseconds = SECDELAY * 1000;
+            Integer in1 = Integer.valueOf(this.input1.getText());
+            Integer in2 = Integer.valueOf(this.input2.getText());
+            final int result = in1 * in2;
+            final JDialog dlg = new JDialog(theFrame, "Progress Dialog", true);
+            final JProgressBar dpb = new JProgressBar(0, milliseconds);
+            dpb.setBorder(new EmptyBorder(10, 10, 10, 10));
+            dlg.add(BorderLayout.CENTER, dpb);
+            JLabel label = new JLabel("Progress...");
+            label.setBorder(new EmptyBorder(10, 10, 10, 0));
+            dlg.add(BorderLayout.NORTH, label);
+            dlg.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+            dlg.setSize(300, 90);
+            dlg.setLocationRelativeTo(theFrame);
+            
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    dlg.setVisible(true);
+                }
+                
+            }).start();
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(100);
+
+                        int totalTime = 0;
+                        
+                        int step = 100;
+                        while (totalTime < milliseconds) {
+                            Thread.sleep(step);
+                            dpb.setValue(dpb.getValue() + step);
+                            totalTime += step;
+                        }
+                    } catch (InterruptedException e) {
+                        //nothing to do 
+                    }
+                    dlg.setVisible(false);
+                    ans.setText("" + result);
+                }
+                
+            }).start();
+            
+        } catch (Exception ex) {
+            //do nothing...
+        }
     }
 
     private static void initLookAndFeel() {
@@ -141,19 +217,41 @@ public class LookAndFeelDemo implements ActionListener {
         JFrame.setDefaultLookAndFeelDecorated(true);
 
         //Create and set up the window.
-        JFrame frame = new JFrame("SwingApplication");
+        JFrame frame = new JFrame("Add Two Numbers");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         LookAndFeelDemo app = new LookAndFeelDemo();
         Component contents = app.createComponents();
         frame.getContentPane().add(contents, BorderLayout.CENTER);
 
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        
         //Display the window.
-        frame.pack();
+        frame.pack();        
+        frame.setLocation(screenWidth / 2 - frame.getWidth() / 2, screenHeight / 2 - frame.getHeight() / 2);
         frame.setVisible(true);
+        LookAndFeelDemo.theFrame = frame;
     }
 
     public static void main(String[] args) {
+        for (String arg : args) {
+            if (arg.equals("old")) {
+                LOOKANDFEEL = "Motif";
+            } else if (arg.equals("new")) {
+                LOOKANDFEEL = "System";
+            } else {
+                try {
+                    SECDELAY = Integer.valueOf(arg);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            
+            
+        }
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
