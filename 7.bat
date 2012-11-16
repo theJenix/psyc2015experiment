20c22f7a661f042d270d131f85f7b5e3e769d858
Jesse Rosalia <jesse.rosalia@gatech.edu>
2012-11-15 23:47:42 -0500

    Fixed 7-12 scripts

--- 7.bat
+++ 7.bat
@@ -1,9 +1,9 @@
 pause
-@call run_new_normal
-@call run_old_fast
-pause
 @call run_new_fast
 @call run_old_normal
+pause
+@call run_new_normal
+@call run_old_fast
 pause
 @call run_new_normal
 @call run_old_normal
